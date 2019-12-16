2437db79466350b94b1b9a50162e2239627c785b
VladislavVB <burdukovskij.vlad@mail.ru>
2019-12-16 22:00:45 +0300

    add: slider, small fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,32 +1,43 @@
-
 $(document).ready(function () {
-    var modal = $('.modal'),
-        modalBtn = $('[data-toggle=modal]'),
-        closeBtn = $('.modal__close');
-    modalBtn.on('click', function () {
-        modal.toggleClass('modal--visible');
-    });
-    closeBtn.on('click', function () {
-        modal.toggleClass('modal--visible');
-    });
+ 
+  var modal = $('.modal'),
+    modalBtn = $('[data-toggle=modal]'),
+    closeBtn = $('.modal__close');
+  
+  modalBtn.on('click', function () {
+    modal.toggleClass('modal--visible');
+  });
 
-    var mySwiper = new Swiper ('.swiper-container', {
-        loop: true,
-        pagination: {
-            el: '.swiper-pagination',
-            type: 'bullets'
-        },
-        navigation:{
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-    });
-    var next = $('.swiper-button-next');
-    var prev = $('.swiper-button-prev');
-    var bullets = $('.swiper-pagination')
+  closeBtn.on('click', function () {
+    modal.toggleClass('modal--visible');
+  });
 
-    next.css('left', prev.widht() + 10 + bullets.widht( + 10))
-    bullets.css('left', prev.widht() + 10 )
+  $(document).keydown(function () {
+    if (event.keyCode == 27) {
+      modal.removeClass('modal--visible');
+    }
+  }); 
 
+  $(document).click(function (event) {
+    $(event.target).toggleClass('modal--visible');
+  });
+
+// slider
+  var mySwiper = new Swiper ('.swiper-container', {
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  })
+  var next = $('.swiper-button-next');
+  var prev = $('.swiper-button-prev');
+  var bullets = $('.swiper-pagination');
+
+  next.css('left', prev.width() + 20 + bullets.width() + 20)
+  bullets.css('left', prev.width() + 20)
 });
-
